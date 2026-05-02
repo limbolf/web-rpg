2b1a9e736a817dc0dcab91b31222320822c9c10e
limbolf <vincentdecoopman0@gmail.com>
2026-05-03 01:30:09 +0200

    Adding exponential buy for Warriors, adding magician, fixing float number with Math.round()

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -4,8 +4,10 @@ import "./App.css";
 function App() {
   const [pv, setPv] = useState(100);
   const [or, setOr] = useState(0);
-  const prixGuerrier = 10;
+  const [prixGuerrier, setPrixGuerrier] = useState(10);
+  const [prixMagicien, setPrixMagicien] = useState(50);
   const [guerrier, setGuerrier] = useState(0);
+  const [magicien, setMagicien] = useState(0);
   const handleAttack = () => {
     setPv((prev) => prev - 10);
   };
@@ -18,19 +20,53 @@ function App() {
   }, [pv]);
 
   const acheterGuerrier = () => {
-    const prix = 10;
-
-    if (or >= prix) {
-      setOr((prevOr) => prevOr - prix);
+    if (or >= prixGuerrier) {
+      setOr((prev) => prev - prixGuerrier);
       setGuerrier((prevGuerriers) => prevGuerriers + 1);
+      setPrixGuerrier((prevPrixGuerrier) => Math.round(prevPrixGuerrier * 1.1));
     } else {
       alert("Pas assez d'or !");
     }
   };
+
+  const acheterMagicien = () => {
+    if (or >= prixMagicien) {
+      setOr((prev) => prev - prixMagicien);
+      setMagicien((prevMagicien) => prevMagicien + 1);
+    } else {
+      alert("Pas assez d'or !");
+    }
+  };
+
+  useEffect(() => {
+    let degatsGuerrier = guerrier * 5;
+    let intervalGuerrier = setInterval(() => {
+      if (degatsGuerrier > 0) {
+        setPv((prev) => prev - degatsGuerrier);
+      }
+    }, 2000);
+    return () => clearInterval(intervalGuerrier);
+  }, [guerrier]);
+
+  useEffect(() => {
+    let degatsMagicien = magicien * 12;
+    let intervalMagicien = setInterval(() => {
+      if (degatsMagicien > 0) {
+        setPv((prev) => prev - degatsMagicien);
+      }
+    }, 4000);
+    return () => clearInterval(intervalMagicien);
+  }, [magicien]);
+
   return (
     <div className="game-app">
       <aside className="shop">
-        <button>Acheter un guerrier : {prixGuerrier} </button>
+        <p>Prix d'un guerrier {prixGuerrier} </p>
+        <button onClick={acheterGuerrier}>Acheter un guerrier</button>
+        <p>Guerriers possédés : {guerrier}</p> <br />
+        <p>Prix d'un magicien {prixMagicien} </p>
+        <button onClick={acheterMagicien}>Acheter un magicien</button>
+        <p>Magiciens possédés : {magicien}</p>
       </aside>
       <div className="battle-floor">
         <button onClick={handleAttack}>Attack</button>
